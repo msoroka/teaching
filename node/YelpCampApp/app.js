cd8154862c00de58bc2bcdbf51784d915f2af3ea
Mateusz Soroka <xmsoroka@gmail.com>
2019-03-21 23:08:42 +0100

    Some styling

--- node/YelpCampApp/app.js
+++ node/YelpCampApp/app.js
@@ -1,18 +1,40 @@
 var express = require("express");
 var app = express();
+var bodyParder = require("body-parser");
+
+app.use(bodyParder.urlencoded({
+    extended: true
+}));
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.render("homepage");
 });
 
-app.get("/campgrounds", function(req,res){
+var campgrounds = [
+    {name: "Tatry", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
+    {name: "Bieszczady", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
+    {name: "Łeba", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
+];
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("campgrounds-new");
+});
+
+app.get("/campgrounds", function (req, res) {
     res.render("campgrounds", {
-        
+        campgrounds: campgrounds,
     });
 });
 
-app.listen(3000, function(){
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+
+    res.redirect("/campgrounds");
+});
+
+app.listen(3000, function () {
     console.log("App is running on port 3000");
 });
