3e3d05222e9772d0e7e5b1a8398f687cb5509db4
Mateusz Soroka <xmsoroka@gmail.com>
2019-03-22 20:43:43 +0100

    Added show route

--- node/YelpCampApp/app.js
+++ node/YelpCampApp/app.js
@@ -1,6 +1,11 @@
-var express = require("express");
-var app = express();
-var bodyParder = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParder = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp", {
+    useNewUrlParser: true,
+});
 
 app.use(bodyParder.urlencoded({
     extended: true
@@ -12,27 +17,63 @@ app.get("/", function (req, res) {
     res.render("homepage");
 });
 
-var campgrounds = [
-    {name: "Tatry", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
-    {name: "Bieszczady", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
-    {name: "Łeba", image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc/mazama-campground.jpg"},
-];
-
-app.get("/campgrounds/new", function (req, res) {
-    res.render("campgrounds-new");
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
 });
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds,
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {
+                campgrounds: allCampgrounds,
+            });
+        }
+    });
+});
+
+app.get("/campgrounds/create", function (req, res) {
+    res.render("campgrounds/create");
+});
+
+app.get("/campgrounds/:id", function (req, res) {
+    var id = req.params.id;
+
+    Campground.findById(id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            console.log(foundCampground);
+            res.render("campgrounds/show", {
+                campground: foundCampground,
+            });
+        }
     });
 });
 
 app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description;
 
-    res.redirect("/campgrounds");
+    Campground.create({
+        name: name,
+        image: image,
+        description: description,
+    }, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds/create");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.listen(3000, function () {
